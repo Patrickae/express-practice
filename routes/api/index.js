28b11396d3dcb1156b38a307b4787b52a63a270a
Andy <andyepatrick@gmail.com>
2020-01-01 19:53:53 -0500

    restructure

--- routes/api/index.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const fbRoutes = require("./fireboard");
-
-// Book routes
-router.use("/probes", fbRoutes);
-
-module.exports = router;
--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 const express = require('express')
 const bodyParser = require("body-parser");
-const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
-
+const axios = require('axios');
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
  
 // parse application/json
 app.use(bodyParser.json())
 
-// Add routes, both API and view
-app.use(routes);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, "/client/build/index.html"));
+});
+
+app.get('api/probes/temps', (req, res) => {
+	axios.get(`https://fireboard.io/api/v1/devices/${process.env.FBDEVICE}/temps.json`, 
+    {headers: {Authorization: `Token ${process.env.FBID}`}})
+  .then((response) => {
+    res.send(response.data)
+  })
+})
 
 // Start the API server
 app.listen(PORT, function() {
